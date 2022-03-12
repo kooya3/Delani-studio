1658e9b7375732f7b32bc16e66569812355c04f4
kooya3 <84116117+kooya3@users.noreply.github.com>
2022-03-13 02:02:21 +0300

    defining functions and assigning variables

---
+++ script.js
@@ -0,0 +1,11 @@
+$(function () {
+    let desp = $('#desp');
+    let devp = $('#devp');
+    let prop = $('#prop');
+    let destext = $('#destxt');
+    let devtext = $('#devtext');
+    let protext = $('#protext');
+
+    $(desp).hide();
+    $(devp).hide();
+    $(prop).hide();
